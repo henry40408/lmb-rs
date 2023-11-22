ed8dda8456746703fbc74493c8c301c5602e281c
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2023-11-22 22:01:19 +0800

    chore: upgrade to rust 1.74.0
    * move lint rules to manifest
    * fix clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,3 @@
-// https://github.com/EmbarkStudios/rust-ecosystem/blob/732513edfd9172f4eda358b2d0cefc6cad1585ee/lints.rs
-#![deny(unsafe_code)]
-#![warn(
-    clippy::all,
-    clippy::await_holding_lock,
-    clippy::char_lit_as_u8,
-    clippy::checked_conversions,
-    clippy::dbg_macro,
-    clippy::debug_assert_with_mut_call,
-    clippy::doc_markdown,
-    clippy::empty_enum,
-    clippy::enum_glob_use,
-    clippy::exit,
-    clippy::expl_impl_clone_on_copy,
-    clippy::explicit_deref_methods,
-    clippy::explicit_into_iter_loop,
-    clippy::fallible_impl_from,
-    clippy::filter_map_next,
-    clippy::flat_map_option,
-    clippy::float_cmp_const,
-    clippy::fn_params_excessive_bools,
-    clippy::from_iter_instead_of_collect,
-    clippy::if_let_mutex,
-    clippy::implicit_clone,
-    clippy::imprecise_flops,
-    clippy::inefficient_to_string,
-    clippy::invalid_upcast_comparisons,
-    clippy::large_digit_groups,
-    clippy::large_stack_arrays,
-    clippy::large_types_passed_by_value,
-    clippy::let_unit_value,
-    clippy::linkedlist,
-    clippy::lossy_float_literal,
-    clippy::macro_use_imports,
-    clippy::manual_ok_or,
-    clippy::map_err_ignore,
-    clippy::map_flatten,
-    clippy::map_unwrap_or,
-    clippy::match_on_vec_items,
-    clippy::match_same_arms,
-    clippy::match_wild_err_arm,
-    clippy::match_wildcard_for_single_variants,
-    clippy::mem_forget,
-    clippy::mismatched_target_os,
-    clippy::missing_enforced_import_renames,
-    clippy::mut_mut,
-    clippy::mutex_integer,
-    clippy::needless_borrow,
-    clippy::needless_continue,
-    clippy::needless_for_each,
-    clippy::option_option,
-    clippy::path_buf_push_overwrite,
-    clippy::ptr_as_ptr,
-    clippy::rc_mutex,
-    clippy::ref_option_ref,
-    clippy::rest_pat_in_fully_bound_structs,
-    clippy::same_functions_in_if_condition,
-    clippy::semicolon_if_nothing_returned,
-    clippy::single_match_else,
-    clippy::string_add,
-    clippy::string_add_assign,
-    clippy::string_lit_as_bytes,
-    clippy::string_to_string,
-    clippy::todo,
-    clippy::trait_duplication_in_bounds,
-    clippy::unimplemented,
-    clippy::unnested_or_patterns,
-    clippy::unused_self,
-    clippy::useless_transmute,
-    clippy::verbose_file_reads,
-    clippy::zero_sized_map_values,
-    future_incompatible,
-    nonstandard_style,
-    rust_2018_idioms
-)]
-
 use std::{
     cell::RefCell,
     io::{BufRead, BufReader, Read},
@@ -146,7 +70,7 @@ pub fn evaluate<R: Read>(e: &mut Evaluation<R>) -> LamResult<EvaluationResult> {
                 if f == "*a" {
                     let mut buf = Vec::new();
                     e.input.borrow_mut().read_to_end(&mut buf)?;
-                    let s = vm.create_string(String::from_utf8(buf).unwrap_or(String::new()))?;
+                    let s = vm.create_string(String::from_utf8(buf).unwrap_or_default())?;
                     return Ok(Value::String(s));
                 }
                 if f == "*l" {
@@ -168,7 +92,7 @@ pub fn evaluate<R: Read>(e: &mut Evaluation<R>) -> LamResult<EvaluationResult> {
                 let mut buf = vec![0; i];
                 let count = e.input.borrow_mut().read(&mut buf)?;
                 buf.truncate(count);
-                let s = vm.create_string(String::from_utf8(buf).unwrap_or(String::new()))?;
+                let s = vm.create_string(String::from_utf8(buf).unwrap_or_default())?;
                 return Ok(Value::String(s));
             }
 
@@ -183,7 +107,7 @@ pub fn evaluate<R: Read>(e: &mut Evaluation<R>) -> LamResult<EvaluationResult> {
             let mut byte_buf = vec![0; 1];
             loop {
                 if expected_read == 0 {
-                    return Ok(String::from_utf8(buf).unwrap_or(String::new()));
+                    return Ok(String::from_utf8(buf).unwrap_or_default());
                 }
                 let read_bytes = e.input.borrow_mut().read(&mut byte_buf)?;
                 // caveat: buffer is not empty when no bytes are read
@@ -191,7 +115,7 @@ pub fn evaluate<R: Read>(e: &mut Evaluation<R>) -> LamResult<EvaluationResult> {
                     buf.extend_from_slice(&byte_buf);
                 }
                 if read_bytes == 0 {
-                    return Ok(String::from_utf8(buf).unwrap_or(String::new()));
+                    return Ok(String::from_utf8(buf).unwrap_or_default());
                 }
                 if std::str::from_utf8(&buf).is_ok() {
                     expected_read -= 1;
