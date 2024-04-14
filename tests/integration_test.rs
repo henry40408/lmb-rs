e1a7c34714fd82bc652a1807b22126ec561d1934
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-04-15 07:57:44 +0800

    test: different newline on windows

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -15,7 +15,10 @@ mod tests {
     fn eval_file() {
         let mut cmd = Command::cargo_bin("lam").unwrap();
         cmd.args(["eval", "--file", "lua-examples/01-hello.lua"]);
+        #[cfg(not(windows))]
         cmd.assert().success().stdout("hello, world!\n");
+        #[cfg(windows)]
+        cmd.assert().success().stdout("hello, world!\r\n");
     }
 
     #[test]
