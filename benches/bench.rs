6959bac9a34b6db1223573c2eb7e7099330d58dc
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-04-18 00:19:41 +0800

    chore: benchmark in CI

--- benches/bench.rs
+++ benches/bench.rs
@@ -3,15 +3,23 @@ use mlua::prelude::*;
 
 use bencher::{benchmark_group, benchmark_main, Bencher};
 
+static SCRIPT: &str = "return true";
+
 fn baseline(bencher: &mut Bencher) {
-    let e = EvalBuilder::new(&b""[..], "return true").build();
+    let e = EvalBuilder::new(&b""[..], SCRIPT).build();
     bencher.iter(|| e.evaluate());
 }
 
-fn original(bencher: &mut Bencher) {
+fn load_eval(bencher: &mut Bencher) {
     let vm = Lua::new();
-    bencher.iter(|| vm.load("return true").eval::<()>());
+    bencher.iter(|| vm.load(SCRIPT).eval::<bool>());
 }
 
-benchmark_group!(evaluation, original, baseline);
+fn call_function(bencher: &mut Bencher) {
+    let vm = Lua::new();
+    let f = vm.load(SCRIPT).into_function().expect("");
+    bencher.iter(|| f.call::<_, bool>(()));
+}
+
+benchmark_group!(evaluation, baseline, call_function, load_eval);
 benchmark_main!(evaluation);
