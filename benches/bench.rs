975284ab0350427bea4a0e74e0114e8cbff58798
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2024-04-19 23:22:34 +0800

    test: benchmark read functions

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::unwrap_used)]
+
 use lam::EvalBuilder;
 use mlua::prelude::*;
 
@@ -17,9 +19,37 @@ fn mlua_load_eval(bencher: &mut Bencher) {
 
 fn mlua_call_function(bencher: &mut Bencher) {
     let vm = Lua::new();
-    let f = vm.load(SCRIPT).into_function().expect("");
+    let f = vm.load(SCRIPT).into_function().unwrap();
     bencher.iter(|| f.call::<_, bool>(()));
 }
 
+fn lam_read_all(bencher: &mut Bencher) {
+    let input = "1";
+    let script = "return require('@lam'):read('*a')";
+    bencher.iter(|| {
+        let e = EvalBuilder::new(input.as_bytes(), script).build();
+        e.evaluate().unwrap();
+    });
+}
+
+fn lam_read_line(bencher: &mut Bencher) {
+    let input = "1";
+    let script = "return require('@lam'):read('*l')";
+    bencher.iter(|| {
+        let e = EvalBuilder::new(input.as_bytes(), script).build();
+        e.evaluate().unwrap();
+    });
+}
+
+fn lam_read_number(bencher: &mut Bencher) {
+    let input = "1";
+    let script = "return require('@lam'):read('*n')";
+    bencher.iter(|| {
+        let e = EvalBuilder::new(input.as_bytes(), script).build();
+        e.evaluate().unwrap();
+    });
+}
+
 benchmark_group!(evaluation, lam_baseline, mlua_load_eval, mlua_call_function);
-benchmark_main!(evaluation);
+benchmark_group!(read, lam_read_all, lam_read_line, lam_read_number);
+benchmark_main!(evaluation, read);
